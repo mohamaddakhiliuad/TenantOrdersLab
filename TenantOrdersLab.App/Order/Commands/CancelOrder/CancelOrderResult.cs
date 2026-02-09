9ec7be0447a85ea5a0e6fd0de9d5847e48a711d8
mohamaddakhiliuad <mohamaddakhiliuad@gmail.com>
2026-02-09 12:08:37 -0500

    refactor(cancel-order): minor updates to result and project configuration

--- TenantOrdersLab.App/Order/Commands/CancelOrder/CancelOrderResult.cs
+++ TenantOrdersLab.App/Order/Commands/CancelOrder/CancelOrderResult.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace TenantOrdersLab.App.Order.Commands.CancelOrder
 {
-    internal class CancelOrderResult
-    {
-    }
+    /// <summary>
+    /// Output boundary for CancelOrder use case.
+    /// </summary>
+    public sealed record CancelOrderResult(int OrderId);
+  
 }
